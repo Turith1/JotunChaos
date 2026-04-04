2e1ee5aeeef4a10e8b5cc52127bc5739361b57d7
NareLock <83192968+NareLock@users.noreply.github.com>
2026-04-03 23:41:01 -0300

    Camera
    Cameras parcialmente resolvidas

--- Assets/Scripts/ClientPlayerMove.cs
+++ Assets/Scripts/ClientPlayerMove.cs
@@ -2,18 +2,23 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.InputSystem;
 using StarterAssets;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
 
 public class ClientPlayerMove : NetworkBehaviour
 {
     [SerializeField] private PlayerInput m_PlayerInput;
     [SerializeField] private StarterAssetsInputs m_StarterAssetsInputs;
     [SerializeField] private ThirdPersonController m_ThirdPersonController;
+    [SerializeField] private CinemachineVirtualCamera m_VC;
 
     private void Awake()
     {
         m_StarterAssetsInputs.enabled = false;
         m_PlayerInput.enabled = false;
         m_ThirdPersonController.enabled = false;
+        GameObject Camera = GameObject.FindGameObjectWithTag("CameraPlayer");
+        m_VC = Camera.GetComponent<CinemachineVirtualCamera>();
     }
 
     public override void OnNetworkSpawn()
@@ -22,6 +27,7 @@ public class ClientPlayerMove : NetworkBehaviour
 
         if (IsOwner)
         {
+            m_VC.Follow = this.gameObject.transform;
             m_StarterAssetsInputs.enabled = true;
             m_PlayerInput.enabled = true;
             m_ThirdPersonController.enabled = true;
