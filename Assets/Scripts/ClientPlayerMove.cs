bd964f4c9d2520c37efb77c3dffc2ae61fe78b55
Turith1 <162712980+Turith1@users.noreply.github.com>
2026-04-07 03:34:00 -0300

    cinemachine camera working in multiplayer

--- Assets/Scripts/ClientPlayerMove.cs
+++ Assets/Scripts/ClientPlayerMove.cs
@@ -2,23 +2,21 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.InputSystem;
 using StarterAssets;
-using Unity.Cinemachine;
-using Unity.VisualScripting;
 
 public class ClientPlayerMove : NetworkBehaviour
 {
     [SerializeField] private PlayerInput m_PlayerInput;
     [SerializeField] private StarterAssetsInputs m_StarterAssetsInputs;
     [SerializeField] private ThirdPersonController m_ThirdPersonController;
-    [SerializeField] private CinemachineVirtualCamera m_VC;
+    [SerializeField] private GameObject m_cineCam;
 
     private void Awake()
     {
         m_StarterAssetsInputs.enabled = false;
         m_PlayerInput.enabled = false;
         m_ThirdPersonController.enabled = false;
+        m_cineCam.SetActive(false);
         GameObject Camera = GameObject.FindGameObjectWithTag("CameraPlayer");
-        m_VC = Camera.GetComponent<CinemachineVirtualCamera>();
     }
 
     public override void OnNetworkSpawn()
@@ -27,10 +25,10 @@ public class ClientPlayerMove : NetworkBehaviour
 
         if (IsOwner)
         {
-            m_VC.Follow = this.gameObject.transform;
             m_StarterAssetsInputs.enabled = true;
             m_PlayerInput.enabled = true;
             m_ThirdPersonController.enabled = true;
+            m_cineCam.SetActive(true);
         }
     }
 }
