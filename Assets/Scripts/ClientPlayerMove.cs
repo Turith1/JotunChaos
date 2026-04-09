9417d4a4c96ede3bd4e240983ce43009c9d6db39
Turith1 <162712980+Turith1@users.noreply.github.com>
2026-04-08 21:58:55 -0300

    Player 2 broken on server authorative try

--- Assets/Scripts/ClientPlayerMove.cs
+++ Assets/Scripts/ClientPlayerMove.cs
@@ -16,7 +16,7 @@ public class ClientPlayerMove : NetworkBehaviour
         m_PlayerInput.enabled = false;
         m_ThirdPersonController.enabled = false;
         m_cineCam.SetActive(false);
-        GameObject Camera = GameObject.FindGameObjectWithTag("CameraPlayer");
+        //GameObject Camera = GameObject.FindGameObjectWithTag("CameraPlayer");
     }
 
     public override void OnNetworkSpawn()
@@ -27,8 +27,26 @@ public class ClientPlayerMove : NetworkBehaviour
         {
             m_StarterAssetsInputs.enabled = true;
             m_PlayerInput.enabled = true;
-            m_ThirdPersonController.enabled = true;
             m_cineCam.SetActive(true);
         }
+        if(IsServer)
+            m_ThirdPersonController.enabled = true;
+    }
+
+    [Rpc(SendTo.Server)]
+    private void UpdateInputServerRpc(Vector2 move, Vector2 look, bool jump, bool sprint)
+    {
+        m_StarterAssetsInputs.MoveInput(move);
+        m_StarterAssetsInputs.LookInput(look);
+        m_StarterAssetsInputs.JumpInput(jump);
+        m_StarterAssetsInputs.SprintInput(sprint);
+    }
+
+    private void LateUpdate()
+    {
+        /*if (!IsOwner)
+            return;*/
+
+        UpdateInputServerRpc(m_StarterAssetsInputs.move, m_StarterAssetsInputs.look, m_StarterAssetsInputs.jump, m_StarterAssetsInputs.sprint);
     }
 }
